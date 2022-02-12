a2391fb76e1db388ab86a187050bfc7a0a443f67
cookie-y <2244242892@qq.com>
2022-02-12 16:20:02 +0800

    init

---
+++ App.vue
@@ -0,0 +1,17 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+@import url("//at.alicdn.com/t/font_2826679_u6r7krhush8.css");
+</style>
---
+++ pages/race/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>比赛</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
