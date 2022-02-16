23d36f7d17e6b05e85c274fb77cc936d1776d768
cookie-y <2244242892@qq.com>
2022-02-16 22:06:48 +0800

    transition

---
+++ pages/index/transition.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="transition">
+		<u-image src="/static/transition.png" :fade="true" duration="450" height="300" width="100"
+			@load="toLogin"/>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				timer: null
+			}
+		},
+		methods: {
+			toLogin() {
+				this.timer = setTimeout(() => {
+					uni.redirectTo({
+						url: "./login"
+					})
+				}, 3000)
+			}
+		},
+		unmounted() {
+			clearTimeout(this.timer)
+		}
+	}
+</script>
+
+<style>
+.transition /deep/ .u-image {
+	margin: 0 auto;
+}
+</style>
